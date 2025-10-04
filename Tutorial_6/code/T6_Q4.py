9576af079cbfbe766b5458c944cb5498e58e5a46
linfengfrank <linfengfrank@gmail.com>
2025-10-04 17:42:30 +0800

    Update the slides and code for Q4 to Tutorial 6

--- Tutorial_6/code/T6_Q4.py
+++ Tutorial_6/code/T6_Q4.py
@@ -6,11 +6,11 @@ y = np.array([1, 1, -1, 1, -1])
 
 ## Linear regression for classification 
 w = inv(X.T @ X) @ X.T @ y 
-print(w) 
+print(f"w = {w}") 
 
-Xt = np.array([[1,6]]) 
+Xt = np.array([[1,-0.1], [1,0.4]]) 
 y_predict = Xt @ w 
-print(y_predict) 
+print(f"y_predict = {y_predict}") 
 
 y_class_predict = np.sign(y_predict) 
-print(y_class_predict)
+print(f"y_class_predict = {y_class_predict}")
